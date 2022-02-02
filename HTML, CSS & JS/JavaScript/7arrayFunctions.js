286a80fe40764646669e13c17125b05bfc751a4a
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-02 16:25:32 +0530

    Array functions

---
+++ HTML, CSS & JS/JavaScript/7arrayFunctions.js
@@ -0,0 +1,8 @@
+//Refer to MDN docs for more Array functions
+// .map() will help you to run a function over elements of an array
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => num * 2);
+
+console.log(numbers);
+console.log(doubleNumArray);
