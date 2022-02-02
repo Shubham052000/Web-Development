e8ec454bcc9e8bd18a079ae2a44ea77d37f8815f
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-02 15:37:47 +0530

    commit

--- HTML, CSS & JS/JavaScript/5destructuring.js
+++ HTML, CSS & JS/JavaScript/5destructuring.js
@@ -1,4 +1,4 @@
-// sExtract array elements or object properties and store them in variables
+// Extract array elements or object properties and store them in variables
 const numbers = [1, 2, 3, 4];
 [num1, num2, , num4] = numbers;
 console.log(num4);
