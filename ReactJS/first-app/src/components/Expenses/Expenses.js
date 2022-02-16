0f0352698ff4211b8d89bda6fa671a82e852a610
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-16 21:52:40 +0530

    Rendering condition content

--- ReactJS/first-app/src/components/Expenses/Expenses.js
+++ ReactJS/first-app/src/components/Expenses/Expenses.js
@@ -15,20 +15,24 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found for the Year selected.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         value={filteredYear}
         onChangeFilter={selectedValueHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
