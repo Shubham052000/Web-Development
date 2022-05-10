41a2d66e3e6e83a4bfbdd383221002e087d4f7ff
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-05-10 21:00:22 +0530

    using http hook for post requests

--- ReactJS/9.1CustomHooks-ToDoList/src/components/NewTask/NewTask.js
+++ ReactJS/9.1CustomHooks-ToDoList/src/components/NewTask/NewTask.js
@@ -1,41 +1,37 @@
-import { useState } from "react";
-
+import { useCallback } from "react";
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
+import useHttp from "../../hooks/use-http";
+
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const createTask = useCallback(
+    (taskText, taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+    },
+    [props]
+  );
+
+  const {
+    isLoading,
+    error,
+    sendRequest: sendTaskRequest,
+  } = useHttp(createTask);
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-174dc-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: "https://react-http-174dc-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
