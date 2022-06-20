859fb583b40f46c50ca58a8220b687c12a6fdd2b
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-06-20 21:37:57 +0530

    Practicing more with Nested Routes

--- ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/pages/QuoteDetail.js
+++ ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import { DUMMY_QUOTES } from "./AllQuotes";
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
@@ -13,6 +13,13 @@ const QuoteDetail = () => {
   return (
     <>
       <HighLightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${param.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${param.quoteId}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${param.quoteId}/comments`}>
         <Comments />
       </Route>
