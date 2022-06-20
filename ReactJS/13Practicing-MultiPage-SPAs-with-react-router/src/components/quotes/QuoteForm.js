095e5386e92520adb55d4d371e04c3c5dd1fa195
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-06-20 21:13:46 +0530

    preventing unwanted route transitions with Promp component

--- ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/components/quotes/QuoteForm.js
+++ ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/components/quotes/QuoteForm.js
@@ -1,12 +1,14 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import classes from "./QuoteForm.module.css";
+import { Prompt } from "react-router-dom";
 
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntered, setIsEntered] = useState("false");
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -18,29 +20,48 @@ const QuoteForm = (props) => {
 
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
-
+  const formFocusHandler = () => {
+    setIsEntered(true);
+  };
+  const finishEnteringHandler = () => {
+    setIsEntered(false);
+  };
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <>
+      <Prompt
+        when={isEntered}
+        message={() =>
+          "Are your sure you want to leave? All your data will be lost"
+        }
+      />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows="5" ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className="btn">Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishEnteringHandler} className="btn">
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
