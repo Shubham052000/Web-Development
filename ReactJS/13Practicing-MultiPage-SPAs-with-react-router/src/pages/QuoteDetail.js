95fc9452d391ba9b8713c0b018cd48d1ef166366
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-06-21 23:13:15 +0530

    more flexible routing code with useRouteMatch hook

--- ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/pages/QuoteDetail.js
+++ ReactJS/13Practicing-MultiPage-SPAs-with-react-router/src/pages/QuoteDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link, Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import { DUMMY_QUOTES } from "./AllQuotes";
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const param = useParams();
   const quote = DUMMY_QUOTES.find((quote) => quote.id === param.quoteId);
   if (!quote) {
@@ -13,14 +14,14 @@ const QuoteDetail = () => {
   return (
     <>
       <HighLightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${param.quoteId}`} exact>
+      <Route path={`${match.path}`} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${param.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.url}comments`}>
             Load comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${param.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
