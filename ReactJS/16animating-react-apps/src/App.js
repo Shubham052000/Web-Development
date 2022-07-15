0ea2aeb0fa4a5574ad3b25b2b0e202d88399c375
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-07-15 19:43:40 +0530

    Starting with animating react componenets with React-Transition-Group

--- ReactJS/16animating-react-apps/src/App.js
+++ ReactJS/16animating-react-apps/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Transition from "react-transition-group/Transition";
+
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,7 +9,7 @@ import List from "./components/List/List";
 
 const App = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-
+  const [showBlock, setShowBlock] = useState(false);
   const showModal = () => {
     setModalIsOpen(true);
   };
@@ -19,8 +21,34 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Animations</h1>
-      <Modal show={modalIsOpen} closed={closeModal} />
-      <Backdrop show={modalIsOpen} />
+      <button onClick={setShowBlock.bind(null, (prevState) => !prevState)}>
+        Toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={1000} mountOnEnter unmountOnExit>
+        {(state) => (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+              transition: "opacity 1000ms ease-out",
+              opacity: state === "exiting" ? 0 : 1,
+            }}
+          ></div>
+        )}
+      </Transition>
+      {/* {showBlock && (
+        
+      )} */}
+
+      {modalIsOpen && (
+        <>
+          <Modal show={modalIsOpen} closed={closeModal} />
+          <Backdrop show={modalIsOpen} />
+        </>
+      )}
       <button className="Button" onClick={showModal}>
         Open Modal
       </button>
