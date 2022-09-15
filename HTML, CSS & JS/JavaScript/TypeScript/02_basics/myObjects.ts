41e1b78355bef9052450dfa81a1a5c40e0ea8ddb
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-09-15 20:47:01 +0530

    TypeScript typeAliases

--- HTML, CSS & JS/JavaScript/TypeScript/02_basics/myObjects.ts
+++ HTML, CSS & JS/JavaScript/TypeScript/02_basics/myObjects.ts
@@ -14,4 +14,17 @@ const createCourse = (): { name: string; price: number } => {
   return { name: "reactjs", price: 399 };
 };
 
+type Book = {
+  name: string;
+  author: string;
+  price: number;
+  isAvailable: boolean;
+};
+
+function createBook(book: Book): Book {
+  return book;
+}
+
+createBook({ name: "", author: "", price: 399, isAvailable: false });
+
 export {};
