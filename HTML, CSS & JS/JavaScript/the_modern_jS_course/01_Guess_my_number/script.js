cdcd38493f06a2d92204ef5d643ad11af76ce030
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-01-24 18:22:55 +0530

    finishing guess my number project adding highscore functionality and implementing DRY methodology

--- HTML, CSS & JS/JavaScript/the_modern_jS_course/01_Guess_my_number/script.js
+++ HTML, CSS & JS/JavaScript/the_modern_jS_course/01_Guess_my_number/script.js
@@ -2,40 +2,39 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+
+const displayMessage = function (messageString) {
+  document.querySelector(".message").textContent = messageString;
+};
 
 const btnCheck = document.querySelector(".check");
 btnCheck.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "🛑 No number";
+    displayMessage("🛑 No number");
   }
   // When player wins
   else if (guess === secretNumber) {
+    if (score > highScore) {
+      highScore = score;
+    }
+    document.querySelector(".highscore").textContent = highScore;
     document.querySelector(".number").textContent = secretNumber;
-    document.querySelector(".message").textContent = "🎉 Correct number";
+    displayMessage("🎉 Correct number");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
   }
-  // When the guess is too high
-  else if (guess > secretNumber) {
+  // When the guess is not correct
+  else if (guess != secretNumber) {
     if (score > 1) {
       score--;
-      document.querySelector(".message").textContent = " Too high!";
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too High!" : "Too Low!";
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "💣 You lost the game";
-      document.querySelector(".score").textContent = 0;
-    }
-  }
-  // When the guess is too low
-  else {
-    if (score > 1) {
-      score--;
-      document.querySelector(".message").textContent = " Too Low!";
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💣 You lost the game";
+      displayMessage("💣 You lost the game");
       document.querySelector(".score").textContent = 0;
     }
   }
@@ -43,9 +42,10 @@ btnCheck.addEventListener("click", function () {
 
 const btnAgain = document.querySelector(".again");
 btnAgain.addEventListener("click", function () {
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = "20";
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector(".number").style.width = "15rem";
