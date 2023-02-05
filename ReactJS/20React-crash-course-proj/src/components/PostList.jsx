0f8a3055ee08fc3dc2ba1c7b30205ecc33d9b413
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-02-05 15:59:35 +0530

    Adding eventListeners setting state and lifting the state up

--- ReactJS/20React-crash-course-proj/src/components/PostList.jsx
+++ ReactJS/20React-crash-course-proj/src/components/PostList.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import Post from "./Post";
 import classes from "./PostLists.module.css";
 import NewPost from "./NewPost";
 
 const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const changeBodyHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+  const changeAuthorHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Shubham" body="React.js is awesome" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Satyawali" body="TS is based" />
       </ul>
     </>
