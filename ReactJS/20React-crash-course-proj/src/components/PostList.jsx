87625eafaab03165382f191c9ca1f9f463ed725d
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-02-09 19:16:52 +0530

    Completing the proj with adding routing and using react-router loaders and actions with dynamic routes

--- ReactJS/20React-crash-course-proj/src/components/PostList.jsx
+++ ReactJS/20React-crash-course-proj/src/components/PostList.jsx
@@ -1,45 +1,20 @@
-import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
 import Post from "./Post";
 import classes from "./PostLists.module.css";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import { useEffect } from "react";
 
-const PostList = ({ modalIsVisible, onCloseModal }) => {
-  const [postData, setPostData] = useState([]);
-
-  useEffect(() => {
-    (async function () {
-      const response = await fetch("http://localhost:8080/posts");
-      const data = await response.json();
-      setPostData(data.posts);
-    })();
-  }, []);
-
-  const addPostHandler = (newPost) => {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(newPost),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPostData((prevPosts) => [newPost, ...prevPosts]);
-  };
+const PostList = () => {
+  const posts = useLoaderData();
 
   return (
     <>
-      {modalIsVisible && (
-        <Modal onClose={onCloseModal}>
-          <NewPost onCancel={onCloseModal} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-      {postData.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
-          {postData.map((post) => {
+          {posts.map((post) => {
             return (
               <Post
-                key={crypto.randomUUID()}
+                key={post.id}
+                id={post.id}
                 author={post.author}
                 body={post.body}
               />
@@ -47,7 +22,7 @@ const PostList = ({ modalIsVisible, onCloseModal }) => {
           })}
         </ul>
       )}
-      {postData.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Try adding some!!</p>
