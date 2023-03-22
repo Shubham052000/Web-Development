abe38cd11344a98169995a295d9282290e022274
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-03-22 15:38:47 +0530

    Redirecting requests, parsing request bodies and using node modules system

--- NodeJS/03-understanding-reqs-res/app.js
+++ NodeJS/03-understanding-reqs-res/app.js
@@ -1,30 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    // res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='input'/><button type='submit'>Submit</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (req.url === "/message" && req.method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302; // for redirection
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>First Page from node server</title></head>");
-  res.write("<body><h1>Hello form my first NodeJS server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ NodeJS/03-understanding-reqs-res/routes.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+function requestHandler(req, res) {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    // res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='input'/><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (error) => {
+        res.statusCode = 302; // for redirection
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Page from node server</title></head>");
+  res.write("<body><h1>Hello form my first NodeJS server</h1></body>");
+  res.write("</html>");
+  res.end();
+}
+
+module.exports = requestHandler;
