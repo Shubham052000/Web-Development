1eaf166c31890a5c49b62836f247c76cf45ed643
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-04-03 20:40:05 +0530

    routing requests using express

--- NodeJS/05-intro-expressjs-app/app.js
+++ NodeJS/05-intro-expressjs-app/app.js
@@ -1,13 +1,12 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // Allows the request to continue to the next middleware in line
+app.use("/add-product", (req, res, next) => {
+  console.log("/Add product");
+  res.send("<h1>The 'Add Product' page </h1>");
 });
-app.use((req, res, next) => {
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>Hello from Express </h1>");
 });
