78af32117d8da389b8229096c85a45b76b133a18
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-04-04 20:32:19 +0530

    Extracting routes and adding 404 route to the app

--- NodeJS/05-intro-expressjs-app/app.js
+++ NodeJS/05-intro-expressjs-app/app.js
@@ -1,23 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/> <button type='submit'>Add Product</button></form>"
-  );
-});
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express </h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
