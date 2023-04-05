fc75fb307dd4402323632fea46bf090a0146a348
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-04-05 20:01:38 +0530

    filtering paths creating and serving html pages & returning a 404 page

--- NodeJS/05-intro-expressjs-app/app.js
+++ NodeJS/05-intro-expressjs-app/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
@@ -7,11 +9,13 @@ const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);
