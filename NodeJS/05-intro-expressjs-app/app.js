1f4ee0fb7e48dc1da24aed2b1b0f4a96e93c1b1a
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-05-01 16:46:04 +0530

    starting with ejs converting all the html files to ejs files and using partials for layout

--- NodeJS/05-intro-expressjs-app/app.js
+++ NodeJS/05-intro-expressjs-app/app.js
@@ -1,20 +1,21 @@
 const path = require("path");
 
 const express = require("express");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+// app.set("view engine", "hbs");
 // app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -27,7 +28,12 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("page-not-found", { pageTitle: "Page not found" });
+  res
+    .status(404)
+    .render("page-not-found", {
+      pageTitle: "Page not found",
+      path: "/djalkdf",
+    });
 });
 
 app.listen(3000);
