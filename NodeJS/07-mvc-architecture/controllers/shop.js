b92646d86e48260a79024358a5f3837b469c63f6
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-05-10 18:58:42 +0530

    Adding the product ID to the path extracting dynaic params and loading product detail data

--- NodeJS/07-mvc-architecture/controllers/shop.js
+++ NodeJS/07-mvc-architecture/controllers/shop.js
@@ -20,6 +20,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log(
+    Product.findById(prodId, (product) => {
+      console.log(product);
+    })
+  );
+  res.redirect("/");
+};
+
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
