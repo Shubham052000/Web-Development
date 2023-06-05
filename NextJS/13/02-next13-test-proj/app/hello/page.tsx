ce1b9e74c49d289c6c8d22bd7afb8a3d852866e1
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-06-05 19:04:47 +0530

    Routing basics Link component useRouter hook and metadata API

--- NextJS/13/02-next13-test-proj/app/hello/page.tsx
+++ NextJS/13/02-next13-test-proj/app/hello/page.tsx
@@ -1,11 +1,26 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+  // const pathName = usePathname();
+  // console.log(pathName);
+  const router = useRouter();
   return (
-    <div>
-      <Link href={"/"}>Go to Home Route</Link>
+    <div className="text-center">
+      <Link href={"/"} className=" underline underline-offset-4">
+        Go to Home Route
+      </Link>
       <h1>you're on hello route!!</h1>
+      <button
+        onClick={() => router.push("/")}
+        className="mt-16 rounded-xl border-2 border-solid border-slate-200 px-4 py-2 duration-500 hover:bg-slate-200 hover:text-black
+        "
+      >
+        Click me to go to home route
+      </button>
     </div>
   );
 };
--- NextJS/13/02-next13-test-proj/app/page.tsx
+++ NextJS/13/02-next13-test-proj/app/page.tsx
@@ -4,11 +4,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Link href={"/hello"}>Go to Hello Route</Link>
-      <h1 className={`${montserrat.className} mb-4 text-2xl`}>Hello World</h1>
-      <h1 className={`${pacifico.className} mb-4 text-2xl`}>Hello World</h1>
-      <h1 className={`${raleway.className} mb-4 text-2xl`}>Hello World</h1>
+    <div className="text-center">
+      <Link href={"/hello"} className="underline underline-offset-4">
+        Go to Hello Route
+      </Link>
+      <h1 className={`${montserrat.className} mb-4 text-2xl`}>
+        Hello World montserrat
+      </h1>
+      <h1 className={`${pacifico.className} mb-4 text-2xl`}>
+        Hello World pacifico
+      </h1>
+      <h1 className={`${raleway.className} mb-4 text-2xl`}>
+        Hello World raleway
+      </h1>
       Raleway
       <h1 className={`${raleway.className} mb-4 text-2xl tracking-widest`}>
         ABCDEFGHIJKLMNOPQRSTUVWXYZ
@@ -16,6 +24,6 @@ export default function Home() {
       <h1 className={`${raleway.className} mb-4 text-2xl tracking-widest`}>
         abcdefghijklmnopqrstuvwxyz
       </h1>
-    </>
+    </div>
   );
 }
