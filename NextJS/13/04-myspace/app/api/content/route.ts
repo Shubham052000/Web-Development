e9ec671c1ecfbfe155e9f99941c6b25d69e9f32a
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-10-11 19:49:33 +0530

    starting with next Auth and adding authentication with Github

--- NextJS/13/04-myspace/app/api/content/route.ts
+++ NextJS/13/04-myspace/app/api/content/route.ts
@@ -31,8 +31,11 @@ const posts = [
   },
 ];
 
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export function GET() {
+export async function GET() {
+  const session = await getServerSession();
+
   return NextResponse.json(posts);
 }
