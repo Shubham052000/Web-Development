02de7b248642d9ea6b1a81a3d27e1071a6bf29be
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2024-09-30 20:45:35 +0530

    Added GifSearch page with Search and FollowOn component

--- giphy-clone/src/pages/Category.jsx
+++ giphy-clone/src/pages/Category.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../context/GifContext";
+import Gif from "../components/Gif";
+import FollowOn from "../components/FollowOn";
+
 const Category = () => {
-  return <div>Category</div>;
+  const { category } = useParams();
+
+  const { gf, filter } = GifState();
+
+  const [results, setResults] = useState([]);
+
+  const fetchCategoryResults = async () => {
+    const { data } = await gf.gifs(category, category);
+
+    setResults(data);
+  };
+
+  useEffect(() => {
+    fetchCategoryResults();
+  }, [category]);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-5 my-4">
+      <div className="w-full sm:w-72">
+        {results.length > 0 && <Gif gif={results[0]} hover={false} />}
+        <span className="text-gray-400 text-sm pt-2">
+          Don't tell it to me, GIF it to me!
+        </span>
+
+        <FollowOn />
+
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
