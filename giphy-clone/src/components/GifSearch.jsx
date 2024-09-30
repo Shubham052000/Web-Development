f625e069fdfe052c26acec6053907fcc82081856
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2024-09-30 12:58:02 +0530

    Added functionality for filter Gif

---
+++ giphy-clone/src/components/GifSearch.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const GifSearch = () => {
+  const [query, setQuery] = useState("");
+  return <div>GifSearch</div>;
+};
+
+export default GifSearch;
--- giphy-clone/src/pages/Home.jsx
+++ giphy-clone/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { GifState } from "../context/GifContext";
 import Gif from "../components/Gif";
+import FilterGif from "../components/FilterGif";
 
 const Home = () => {
   const { gf, gifs, setGifs, filter } = GifState();
@@ -26,7 +27,7 @@ const Home = () => {
         alt="earth banner"
         className="mt-2 rounded w-full"
       />
-      <FilterGif />
+      <FilterGif showTrending={true} />
 
       <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
         {gifs.map((gif) => (
